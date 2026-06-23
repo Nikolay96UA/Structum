b12ea4f25b9b2cbbac6f04aa41d1db81a09393fc
Mykola Maloshenko <115625550+Nikolay96UA@users.noreply.github.com>
2026-06-23 07:31:14 +0300

    read add qr-code scanner

--- js/newQr.js
+++ js/newQr.js
@@ -1,25 +1,23 @@
 const generateBtn = document.getElementById('generate');
 const downloadBtn = document.getElementById('download');
+let namePng = ``;
 
 const generateQRCode = () => {
   const text = document.getElementById('text-url').value.trim();
-
-  const encodedText =  unescape(encodeURIComponent(text)); 
+  const encodedText = unescape(encodeURIComponent(text));
 
   if (!text) {
-    alert('Введите URL или текст,пожалуйста');
+    alert('Введите URL или текст, пожалуйста');
     return;
   }
 
-  const size = parseInt(document.querySelector("input[name='qr-size']:checked").value) || 300;
-
+  const size = 300;
   const canvas = document.getElementById('qrcode');
   const ctx = canvas.getContext('2d');
-
+  
   canvas.width = size;
   canvas.height = size;
-
-  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   const qr = new QRious({
     level: 'H',
@@ -29,26 +27,23 @@ const generateQRCode = () => {
 
   const img = new Image();
   img.src = qr.toDataURL();
-
+  
+  // Ждём, пока картинка загрузится в память
   img.onload = () => {
-    ctx.drawImage(img,0,0,size,size)
+    ctx.drawImage(img, 0, 0, size, size);
+    
+    // Переносим вызов скачивания СЮДА
+    namePng = text;
+    downloadQRCode(); 
   };
-
-}
-
+};
 
 const downloadQRCode = () => {
   const canvas = document.getElementById('qrcode');
   const link = document.createElement('a');
-
   link.href = canvas.toDataURL('image/png');
-  link.download = 'qrcode.png';
-
+  link.download = `${namePng}.png`;
   link.click();
-
 };
 
-
-generateBtn.addEventListener('click',generateQRCode);
-downloadBtn.addEventListener('click',downloadQRCode);
-
+generateBtn.addEventListener('click', generateQRCode);
