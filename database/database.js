883a80155954cefdfed9832d4b1b32efa036706f
Mykola Maloshenko <115625550+Nikolay96UA@users.noreply.github.com>
2026-06-29 09:42:52 +0300

    read

--- database/database.js
+++ database/database.js
@@ -6,7 +6,6 @@ import * as XLSX from 'xlsx'; // Добавили импорт для чтени
 import ExcelJS from 'exceljs'; // ✅ Теперь всё в едином ES-стиле
 import nodemailer from 'nodemailer';
 import {Resend} from 'resend';
-import express from 'express';
 const app = express();
 
 // Указываем папку со статическими файлами (HTML, CSS, JS фронтенда)
